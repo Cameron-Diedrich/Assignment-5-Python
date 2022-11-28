c0e8b4834412a474723f328e0193ec60848c36b7
Cameron diedrich <cameron.diedrich@mths.ca>
2022-11-28 16:13:35 +0000

    change

--- hcf.py
+++ hcf.py
@@ -4,3 +4,28 @@
 # Created on: Nov 2022
 # This program finds the Highest Common Factor
 
+import math
+
+def main():
+    # Input
+    firstNumber = input("Enter first number: ")
+    secondNumber = input("Enter second number: ")
+
+
+    # process
+    if firstNumber > secondNumber:
+        largeNumber = firstNumber
+        smallNumber = secondNumber
+    else:
+        largeNumber = secondNumber
+        smallNumber = firstNumber
+
+        largeNumber / smallNumber
+
+
+    print ("the highest common factor is {0}.".format(largeNumber))
+
+    print("\nDone.")
+
+if __name__ == "__main__":
+    main()
